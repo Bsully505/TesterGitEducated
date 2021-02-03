799538bd91c2022920fc5f9ea1f3824672f24cdb
Bryan Sullivan <Bsully505@yahoo.com>
2021-02-02 19:32:58 -0500

    added the first number input for the postfix calculator

--- src/PostfixCalculator.java
+++ src/PostfixCalculator.java
@@ -1,9 +1,21 @@
-
+import java.util.Scanner;
 public class PostfixCalculator {
-
+public static boolean firstNumFlag = true;
+public static int num1 = -1;
 
 
 public static void main(String[] args){
+	Scanner input = new Scanner(System.in);
 	System.out.println("Welcome to my Computer");
+	System.out.println("please enter your first number");
+	while(firstNumFlag)
+	try{
+	num1 = input.nextInt();
+	firstNumFlag = false;
+	}catch(Exception InputMismatchException){
+		System.out.println("Please enter a number");
+		input.next();
+	}
+	System.out.println(num1);
 }
 }
