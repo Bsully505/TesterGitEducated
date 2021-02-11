aff2305df31d9511188e0d065bbb182495f6730f
Bryan Sullivan <Bsully505@yahoo.com>
2021-02-10 23:14:54 -0500

    Added a new file which would be used as a input field

--- src/PostfixCalculator.java
+++ src/PostfixCalculator.java
@@ -1,33 +1,99 @@
 import java.util.Scanner;
+import java.util.Stack;
 public class PostfixCalculator {
 public static boolean firstNumFlag = true;
 public static boolean secondNumFlag = true;
+public static Stack num_Container = new Stack();
 public static int num1 = -1;
 public static int num2 = -1;
 
 
 public static void main(String[] args){
 	Scanner input = new Scanner(System.in);
-	System.out.println("Welcome to my Computer");
-	System.out.println("please enter your first number");
-	while(firstNumFlag){
-	try{
-	num1 = input.nextInt();
-	firstNumFlag = false;
-	}catch(Exception InputMismatchException){
-		System.out.println("Please enter a number");
-		input.next();
-	}
-	}
-	System.out.println("Please enter your second number number");
-	while(secondNumFlag)
-		try{
-		num2 = input.nextInt();
-		secondNumFlag = false;
-		}catch(Exception InputMismatchException){
-			System.out.println("Please enter your second number number");
-			input.next();
+	System.out.println("Welcome to my Calculator");
+	System.out.println("please enter a number");
+	while(true){
+		String temp = input.next();
+		switch(temp){
+		case("+"):
+			if(num_Container.size()>=2){
+				int num1 = (int) (num_Container.pop());
+				int num2 = (int) (num_Container.pop());
+				num_Container.push(num1+num2);
+				ShowStackdata();
+				
+			}
+			else{
+				System.out.println("there are enough numbers in the stack");
+			}
+			break;
+		case("*"):
+			if(num_Container.size()>=2){
+				int num1 = (int) (num_Container.pop());
+				int num2 = (int) (num_Container.pop());
+				num_Container.push(num1*num2);
+				ShowStackdata();
+				
+			}
+			else{
+				System.out.println("there are enough numbers in the stack");
+			}
+			break;
+		case("-"):
+			if(num_Container.size()>=2){
+				int num1 = (int) (num_Container.pop());
+				int num2 = (int) (num_Container.pop());
+				num_Container.push(num2-num1);
+				ShowStackdata();
+			}
+			else{
+				System.out.println("there are enough numbers in the stack");
+			}
+			break;
+		case("/"):
+			if(num_Container.size()>=2){
+				int num1 = (int) (num_Container.pop());
+				int num2 = (int) (num_Container.pop());
+				num_Container.push(num2/num1);
+				ShowStackdata();
+			}
+			else{
+				System.out.println("there are enough numbers in the stack");
+			}
+		break;
+		default:
+			Boolean flag = true;
+			while(flag){
+				try{
+					int newNum = Integer.parseInt(temp);
+					num_Container.push(newNum);
+					flag = false;
+					ShowStackdata();
+				}catch (Exception e){
+					System.out.println("you did not correctly enter a number or symbol");
+				}
+			}
 		}
-	System.out.println(num1 + num2);
+		
+		
+			
+	}
+	
+	}
+public static void ShowStackdata(){
+	 Stack TempContainer = new Stack();
+	 while(num_Container.isEmpty() == false){
+		 int numtemp = (int) num_Container.pop();
+		 System.out.format("%d, ", numtemp);
+		 TempContainer.push(numtemp);
+		 
+	 }
+	 System.out.println();
+	 while(TempContainer.isEmpty() == false){
+		 num_Container.push(TempContainer.pop());
+		 
+	 }
+	
 }
+
 }
